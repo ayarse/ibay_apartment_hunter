11193522bb80400ba1f43c044e0926444a2e255b
Ayas Nasih <ayasnasih@gmail.com>
2025-08-22 17:21:13 +0500

    feat: improve shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import http from 'node:http';
 import * as Sentry from '@sentry/node';
-import { bot as tg } from './clients';
+import { bot as tg } from './telegram';
 import { env } from './config';
 import { logger } from './util/logger';
+import { client } from './db';
 
 import './listeners';
 import { IBayScraper } from './scrapers/ibay';
@@ -33,7 +34,9 @@ const scraperFns = [() => ibayScraper.getUpdates()];
 
 const scraperInt = setInterval(() => {
   logger.info('Running scrapers...');
-  scraperFns.forEach((fn) => fn());
+  scraperFns.forEach((fn) => {
+    fn();
+  });
 }, minsToMs(env.TIME_INTERVAL));
 
 tg.catch((err) => {
@@ -41,21 +44,55 @@ tg.catch((err) => {
   Sentry.captureException(err);
 });
 
-const stopTg = async () => {
-  notifyAdmin('Bot is shutting down...');
-  clearInterval(scraperInt);
-  await tg.stop();
-};
-
-http
+const server = http
   .createServer((_, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OK');
   })
   .listen(process.env.PORT || 3000);
 
-process.once('SIGINT', stopTg);
-process.once('SIGTERM', stopTg);
+let isShuttingDown = false;
+
+const shutdown = async (signal: string) => {
+  if (isShuttingDown) {
+    logger.warn('Already shutting down, forcing exit...');
+    process.exit(1);
+  }
+
+  isShuttingDown = true;
+  logger.info(`Received ${signal}, starting graceful shutdown...`);
+
+  try {
+    await notifyAdmin('Bot is shutting down...');
+
+    clearInterval(scraperInt);
+
+    logger.info('Stopping Telegram bot...');
+    await tg.stop();
+
+    logger.info('Closing HTTP server...');
+    await new Promise<void>((resolve, reject) => {
+      server.close((err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+
+    logger.info('Closing database connection...');
+    await client.end();
+
+    logger.info('Graceful shutdown completed');
+    process.exit(0);
+  } catch (error) {
+    logger.error('Error during graceful shutdown:', error);
+    Sentry.captureException(error);
+    process.exit(1);
+  }
+};
+
+['SIGINT', 'SIGTERM'].forEach((signal) => {
+  process.once(signal, () => shutdown(signal));
+});
 
 process.on('uncaughtException', (err) => {
   logger.error(err);
