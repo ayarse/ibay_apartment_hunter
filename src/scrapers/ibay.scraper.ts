00478357e13c4efd5f5237d5d6e8196a73acb94d
Ayas Nasih <ayasnasih@gmail.com>
2025-08-23 00:47:59 +0500

    refactor: ibay scraper

--- src/scrapers/ibay.scraper.ts
+++ src/scrapers/ibay.scraper.ts
@@ -1,4 +1,4 @@
-import { CheerioCrawler, Configuration } from 'crawlee';
+import { CheerioCrawler, Configuration, CheerioRoot} from 'crawlee';
 import { ConfigService } from '@/services';
 import type { Listing } from '@/util/types';
 import { Events, Locations } from '@/util/types';
@@ -6,27 +6,38 @@ import { trimObjectValues } from '@/util';
 import { eventBus } from '@/events';
 import { env } from '@/config';
 
-const ibayBaseUrl = process.env.IBAY_BASE_URL ?? 'https://ibay.com.mv';
+const BASE_URL = process.env.IBAY_BASE_URL ?? 'https://ibay.com.mv';
 
-const IBAY_URLS = {
-  [Locations.All]: `${ibayBaseUrl}/index.php?page=search&s_res=AND&cid=25&off=0&lang=&s_by=hw_added`,
-  [Locations.Male]: `${ibayBaseUrl}/index.php?page=search&s_res=AND&cid=25&off=0&lang=&s_by=hw_added&reg1_ex=11&reg2_ex=100`,
-  [Locations.Hulhumale]: `${ibayBaseUrl}/index.php?page=search&s_res=AND&cid=25&s_by=hw_added&f_location_ex=Male+--+HulhuMale`,
-  [Locations.Villigili]: `${ibayBaseUrl}/index.php?page=search&s_res=AND&cid=25&s_by=hw_added&f_location_ex=Male+--+Villingili`,
-};
+const LOCATION_URLS = {
+  [Locations.All]: `${BASE_URL}/index.php?page=search&s_res=AND&cid=25&off=0&lang=&s_by=hw_added`,
+  [Locations.Male]: `${BASE_URL}/index.php?page=search&s_res=AND&cid=25&off=0&lang=&s_by=hw_added&reg1_ex=11&reg2_ex=100`,
+  [Locations.Hulhumale]: `${BASE_URL}/index.php?page=search&s_res=AND&cid=25&s_by=hw_added&f_location_ex=Male+--+HulhuMale`,
+  [Locations.Villigili]: `${BASE_URL}/index.php?page=search&s_res=AND&cid=25&s_by=hw_added&f_location_ex=Male+--+Villingili`,
+} as const;
+
+const SELECTORS = {
+  listings: '.bg-light.latest-list-item',
+  title: 'h5 > a',
+  url: 'h5 > a',
+  price: '.col.s8 > .price',
+  id: (data: string) => {
+    const match = /o([0-9]+)\.html/.exec(data);
+    return match?.[1];
+  },
+} as const;
 
 export class IBayScraper {
   public async getUpdates() {
-    const urls = Object.values(IBAY_URLS);
+    const urls = Object.values(LOCATION_URLS);
 
     const crawler = new CheerioCrawler(
       {
         async requestHandler({ request, $ }) {
-          const listings = $('.bg-light.latest-list-item');
+          const listings = $(SELECTORS.listings);
           const listingData: Listing[] = [];
           const currentUrl = request.url;
-          const location = Object.keys(IBAY_URLS).find(
-            (key) => IBAY_URLS[key] === currentUrl,
+          const location = Object.keys(LOCATION_URLS).find(
+            (key) => LOCATION_URLS[key] === currentUrl,
           );
           const configKey = `ibay_latest_item_id_${location}`;
           const currentLatest = await ConfigService.getConfigByKey(configKey);
@@ -34,10 +45,10 @@ export class IBayScraper {
           console.log('Current location', location);
 
           listings.each((_index, el) => {
-            const title = $(el).find('h5 > a').text();
-            const url = $(el).find('h5 > a').attr('href');
-            const price = $(el).find('.col.s8 > .price').text();
-            const id = /o([0-9]+)\.html/.exec(url as string)?.[1];
+            const title = $(el).find(SELECTORS.title).text();
+            const url = $(el).find(SELECTORS.url).attr('href');
+            const price = $(el).find(SELECTORS.price).text();
+            const id = SELECTORS.id(url);
 
             const data = trimObjectValues({
               id,
